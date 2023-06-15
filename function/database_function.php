e229c458ac7d9505ba04c85781c983ea6bf35248
Weng-john <59682713+Weng-john@users.noreply.github.com>
2023-06-15 17:27:20 +0800

    merge

--- function/database_function.php
+++ function/database_function.php
@@ -1,6 +1,6 @@
 <?php
     function db_connect(){
-		$conn = mysqli_connect("localhost", "root", "reeper30226", "final_project");
+		$conn = mysqli_connect("localhost", "root", "john2543", "final_project");
 		if(!$conn){
 			echo "無法使用資料庫" . mysqli_connect_error($conn);
 			exit;
